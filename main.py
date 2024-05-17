b0608f307164321f3116d94bf64d75cd0d353ed7
Rosa-Palaci <palacibelrose431@gmail.com>
2024-05-16 22:10:02 -0600

    Configurar páginas de error

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 
 todos = ['Comprar cafe', 'Enviar cafe', 'Envio a productor']
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
